a7cb41e07faef9f4de163fc3bc4743eb355fb860
chrispydev <christianowusu44@gmail.com>
2021-04-03 01:59:41 +0000

    fonts portfolio border

--- src/Components/portfolio/Portfolio.js
+++ src/Components/portfolio/Portfolio.js
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
-import { MyPortfolioContext } from "../statedata";
-import PortfolioLeft from "./PortfolioLeft";
-import PortfolioRight from "./PortfolioRight";
-import "./Portfolio.css";
-import "./PortfolioContent.css";
+import React, { useContext } from 'react';
+import { MyPortfolioContext } from '../statedata';
+import PortfolioLeft from './PortfolioLeft';
+// import PortfolioRight from './PortfolioRight';
+import './Portfolio.css';
+import './PortfolioContent.css';
 
 function Portfolio() {
   const portfolio = useContext(MyPortfolioContext);
@@ -12,13 +12,16 @@ function Portfolio() {
     <section className="portfolio mt-5" id="portfolio">
       <h1>Portfolio</h1>
       <h4>check what I've been doing lately</h4>
-      <div>
-        <PortfolioLeft portfolio={portfolio.port1} />
-        <PortfolioRight portfolio={portfolio.port2} />
-        <PortfolioLeft portfolio={portfolio.port5} />
-        <PortfolioRight portfolio={portfolio.port4} />
-        <PortfolioRight portfolio={portfolio.port3} />
-      </div>
+      {/* <div> */}
+      <PortfolioLeft portfolio={portfolio.port1} />
+      <PortfolioLeft portfolio={portfolio.port2} />
+      <PortfolioLeft portfolio={portfolio.port3} />
+      <PortfolioLeft portfolio={portfolio.port4} />
+      {/* <PortfolioRight portfolio={portfolio.port2} /> */}
+      <PortfolioLeft portfolio={portfolio.port5} />
+      {/* <PortfolioRight portfolio={portfolio.port4} /> */}
+      {/* <PortfolioRight portfolio={portfolio.port3} /> */}
+      {/* </div> */}
     </section>
   );
 }
--- src/Components/portfolio/PortfolioLeft.js
+++ src/Components/portfolio/PortfolioLeft.js
@@ -1,52 +1,50 @@
-import React from "react";
-import { Row, Container, Col, Button } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import { Row, Container, Col, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function PortfolioItems({ portfolio }) {
-    const portfolioImage = `/images/${portfolio.pic}`;
+  const portfolioImage = `/images/${portfolio.pic}`;
 
-    return (
-        <Container className="center__content mx-auto">
-            <span>
-                <i>{portfolio.info}</i>
-            </span>
+  return (
+    <Container className="center__content mx-auto">
+      <div className="border__container">
+        <p className="skewY__style">
+          <i>{portfolio.info}</i>
+        </p>
 
-            <Row className="portfolio__row">
-                <Col sm={10} md={5} lg={5} className="portfolio__img">
-                    <img src={portfolioImage} alt="port1" />
-                </Col>
-                <Col sm={10} md={5} lg={5} className="portfolio__text">
-                    <p>{portfolio.detail}</p>
-                    <h5 className="portfolio__info">{portfolio.description}</h5>
-                    <div className="flex__items">
-                        <Button
-                            variant="outline-dark"
-                            size="lg"
-                            className="portfolio__btn up"
-                            href={portfolio.visit}
-                            target="_blank"
-                        >
-                            Visit
-                        </Button>
-                        <Button
-                            variant="outline-dark"
-                            size="lg"
-                            className="portfolio__btn1 up"
-                            href={portfolio.source}
-                        >
-                            <FontAwesomeIcon
-                                icon={faGithub}
-                                size="1x"
-                                className="mr-1"
-                            />
-                            Source
-                        </Button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+        <Row className="portfolio__row">
+          <Col sm={10} md={5} lg={5} className="portfolio__img">
+            <img src={portfolioImage} alt="port1" />
+          </Col>
+          <Col sm={10} md={5} lg={5} className="portfolio__text">
+            <p>{portfolio.detail}</p>
+            <h5 className="portfolio__info">{portfolio.description}</h5>
+            <div className="flex__items">
+              <Button
+                variant="outline-dark"
+                size="lg"
+                className="portfolio__btn up"
+                href={portfolio.visit}
+                target="_blank"
+              >
+                Visit
+              </Button>
+              <Button
+                variant="outline-dark"
+                size="lg"
+                className="portfolio__btn1 up"
+                href={portfolio.source}
+              >
+                <FontAwesomeIcon icon={faGithub} size="1x" className="mr-1" />
+                Source
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
 }
 
 export default PortfolioItems;
