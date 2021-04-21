fa4dfc3150d6d4cd71acf6e9095d311ed74cf0e0
chrispydev <christianowusu44@gmail.com>
2021-04-21 19:39:00 +0000

    4k screen fixed

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ import Portfolio from './Components/portfolio/Portfolio';
 import Contact from './Components/contact/Contact';
 import './index.css';
 import Fonts from './Fonts';
+import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
+
+const Wrapper = styled(Container)`
+  max-width: 1440px !important;
+`;
 
 function App() {
   return (
     <>
-      <Fonts />
-      <Header />
-      <Home />
-      <About />
-      <Portfolio />
-      <Contact />
+      <Wrapper className="mx-auto">
+        <Fonts />
+        <Header />
+        <Home />
+        <About />
+        <Portfolio />
+        <Contact />
+      </Wrapper>
     </>
   );
 }
