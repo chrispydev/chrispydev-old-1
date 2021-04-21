e52bd16b0d01808ce21b44281a8a49a8e85d9e83
chrispydev <christianowusu44@gmail.com>
2021-04-21 19:03:57 +0000

    fonts fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import "./Components/media/media-queries.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import About from "./Components/about/About";
-import Header from "./Components/header/Header";
-import Home from "./Components/home/Home";
-import Portfolio from "./Components/portfolio/Portfolio";
-import Contact from "./Components/contact/Contact";
-import "./index.css"
+import React from 'react';
+import './Components/media/media-queries.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './Components/about/About';
+import Header from './Components/header/Header';
+import Home from './Components/home/Home';
+import Portfolio from './Components/portfolio/Portfolio';
+import Contact from './Components/contact/Contact';
+import './index.css';
+import Fonts from './Fonts';
 
 function App() {
-    return (
-        <>
-            <Header />
-            <Home />
-            <About />
-            <Portfolio />
-            <Contact />
-        </>
-    );
+  return (
+    <>
+      <Fonts />
+      <Header />
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
